27b48d2b4f2402558b4c327c0726ce5a18222013
Melissa Evelyn <melissaevelyn28@gmail.com>
2024-10-10 16:46:00 -0300

    Desafio Sistema Celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "12345678", modelo: "modelo 1", imei: "5555555", memoria: 128); 
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartphoneIphone: ");
+Smartphone iphone = new Iphone(numero: "87654321", modelo: "modelo 2", imei: "44444444", memoria: 256); 
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
